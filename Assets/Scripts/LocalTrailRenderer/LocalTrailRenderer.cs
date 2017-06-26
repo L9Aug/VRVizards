4ff65ecf6968dfae61154778940720d164d85dc5
Vive_Uni <up698897@myport.ac.uk>
2017-06-26 11:42:56 +0100

    Local Space Trail inplace and working

---
+++ Assets/Scripts/LocalTrailRenderer/LocalTrailRenderer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Based off of implmentation found here: https://forum.unity3d.com/threads/trail-renderer-local-space.97756/
+*/
+
+[RequireComponent(typeof(LineRenderer))]
+public class LocalTrailRenderer : MonoBehaviour
+{
+
+    /// <summary>
+    /// The object that we want to draw a trail for.
+    /// </summary>
+    [Tooltip("The object that we want to draw a trail for.")]
+    public Transform Target;
+
+    /// <summary>
+    /// The smallest distance required before adding a new point.
+    /// </summary>
+    [Tooltip("The smallest distance required before adding a new point.")]
+    public float Resolution = 0.1f;
+
+    private LineRenderer myLine;
+    private Vector3 LastPos;
+
+    public Vector3 GetImageCenter()
+    {
+        Vector3 Center = Vector3.zero;
+
+        for(int i = 0; i < myLine.positionCount; ++i)
+        {
+            Center += myLine.GetPosition(i);
+        }
+
+        return Center / (float)myLine.positionCount;
+    }
+
+    public float GetImageSize()
+    {
+        float size = 0;
+        Vector3 Center = GetImageCenter();
+
+        for(int i = 0; i < myLine.positionCount; ++i)
+        {
+            float Dist = Vector3.Distance(Center, myLine.GetPosition(i));
+            if(Dist > size)
+            {
+                size = Dist;
+            }
+        }
+        print(size);
+        return size;
+    }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        myLine = GetComponent<LineRenderer>();
+        myLine.useWorldSpace = false;
+        Reset();
+    }
+
+    // put the line renderer back to it's starting state
+    private void Reset()
+    {
+        myLine.positionCount = 0;
+        AddPoint(Target.localPosition);
+    }
+
+    private void OnEnable()
+    {
+        // upon re-enabling this component reset the line renderer
+        myLine.enabled = true;
+        Reset();
+    }
+
+    private void OnDisable()
+    {
+        // when this object is disabled also turn off the line renderer
+        myLine.enabled = false;
+    }
+
+    // Add a point to the line renderer
+    private void AddPoint(Vector3 newPoint)
+    {
+        ++myLine.positionCount;
+        myLine.SetPosition(myLine.positionCount - 1, newPoint);
+        LastPos = newPoint;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 CurrentPos = Target.localPosition;
+        if(Vector3.Distance(CurrentPos, LastPos) >= Resolution)
+        {
+            AddPoint(CurrentPos);
+        }
+    }
+}
--- Assets/Scripts/Player/TrailController.cs
+++ Assets/Scripts/Player/TrailController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 using FSM;
 using Condition;
 
-[RequireComponent(typeof(TrailRenderer))]
+[RequireComponent(typeof(LocalTrailRenderer))]
 public class TrailController : MonoBehaviour
 {
 
     StateMachine SM;
-    TrailRenderer TR;
+    LocalTrailRenderer TR;
+    public Transform HMD;
     public Camera orthoCam;
     bool isDrawing;
     public Texture2D capturedImage;    
@@ -18,7 +19,7 @@ public class TrailController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        TR = GetComponent<TrailRenderer>();
+        TR = GetComponent<LocalTrailRenderer>();
         InitStateMachine();
     }
 
@@ -27,7 +28,7 @@ public class TrailController : MonoBehaviour
         BoolCondition startDrawingCon = new BoolCondition(GetIsDrawing);
         NotCondition endDrawingCon = new NotCondition(startDrawingCon);
 
-        Transition start = new Transition("Start Drawing", startDrawingCon, new List<Action> { PlaceCamera, EnableTrail, TR.Clear });
+        Transition start = new Transition("Start Drawing", startDrawingCon, new List<Action> { PlaceCamera, EnableTrail });
         Transition end = new Transition("Finish Drawing", endDrawingCon, new List<Action> { Snapshot, DisableTrail });
 
         State idle = new State("Idle", new List<Transition> { start }, new List<Action> {  }, new List<Action> { }, new List<Action> { });
@@ -54,6 +55,7 @@ public class TrailController : MonoBehaviour
 
     IEnumerator TakeSnapshot()
     {
+        PositionCamera();
         yield return new WaitForEndOfFrame();
         capturedImage = new Texture2D(256, 256, TextureFormat.RGBA32, false);
         RenderTexture.active = orthoCam.targetTexture;
@@ -65,6 +67,13 @@ public class TrailController : MonoBehaviour
         File.WriteAllBytes("D:/up690813/VRVizards/VRVizards/TrainingImages/Test.png", bytes );
         Debug.Log("Written to file");
         orthoCam.gameObject.SetActive(false);
+    }
+
+    void PositionCamera()
+    {
+        orthoCam.orthographicSize = TR.GetImageSize();
+        orthoCam.transform.position = HMD.position;
+        orthoCam.transform.LookAt(TR.GetImageCenter());
     }
 
     bool GetIsDrawing()
